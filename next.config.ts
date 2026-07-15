a167267eea44ad9723ec92df55b96113c6387be0
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-07-15 19:58:35 +0530

    financial dashboard

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  devIndicators : {
+    position : "bottom-right",
+  }, //to hide it :- devIndicators : false
+};
+
+export default nextConfig;
